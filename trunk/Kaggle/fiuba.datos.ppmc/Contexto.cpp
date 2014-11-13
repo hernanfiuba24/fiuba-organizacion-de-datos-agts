ee9688a479312be5326e33238e482db9cb1e45d5
hernanfiuba2009@gmail.com <hernanfiuba2009@gmail.com@a4a10ecf-ccfd-542f-24b2-f42e9852f183>
2014-11-13 17:39:09 +0000

    Se agrego el hasheo a las Palabras en Contexto.

--- trunk/Kaggle/fiuba.datos.ppmc/Contexto.cpp
+++ trunk/Kaggle/fiuba.datos.ppmc/Contexto.cpp
@@ -2,20 +2,26 @@
 
 using namespace std;
 
-Contexto::Contexto(string unNombre){
+Contexto::Contexto(string unNombre, int primo){
 	this->nombre = unNombre;
 	this->palabras = new list<Palabra*>;
+	this->unHash = new HashingUniversalS(primo);
 }
 
 void Contexto::agregarPalabra(Palabra* unaPalabra){
+	int indice = this->hashearPalabra(unaPalabra);
+
 	std::list<Palabra*>::iterator it;
     it = this->palabras->begin();
-    it++;
+    advance(it,indice);
 	this->palabras->insert(it, unaPalabra);
 }
 
 string Contexto::getNombre(){
 	return this->nombre;
+}
+int Contexto::hashearPalabra(Palabra* unaPalabra){
+	return (this->unHash->hashString(unaPalabra->getNombre()));
 }
 
 Contexto::~Contexto() {
