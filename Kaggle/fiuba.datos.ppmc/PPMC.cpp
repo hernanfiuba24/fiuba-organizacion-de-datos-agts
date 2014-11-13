8da15113e9992a36a2c0e85881615ad541959b9e
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@a4a10ecf-ccfd-542f-24b2-f42e9852f183>
2014-11-13 19:16:57 +0000

    - Se completo el metodo entrenarPalabras (aka entrenarModelos), falta controlar error en devolerCincoPalabras.

--- Kaggle/fiuba.datos.ppmc/PPMC.cpp
+++ Kaggle/fiuba.datos.ppmc/PPMC.cpp
@@ -14,19 +14,25 @@ PPMC::PPMC(int cantidadDeModelos) {
 	this->modelo0 = new Modelo0(5001);
 	this->modelo1 = new Modelo1(7001);
 	this->modelosSuperiores = new list<ModelosSuperiores*>;
-	for(int i = 2; i<=(cantidadDeModelos); i++){
-		int primo;
-		if (i == 2)
-			primo = 3001;
-		else if (i == 3)
-			primo = 6001;
-		else if (i == 4)
-			primo = 7001;
-		else if (i == 5)
-			primo = 8001; //ES POCO ESPACIO. 8001 SON MENOS DE 150K (SE PUDE AGREGAR MUCHO MAS)
-		ModelosSuperiores* modeloi = new ModelosSuperiores(i, primo);
-		this->modelosSuperiores->push_back(modeloi);
+	for(int numeroDeModelo = 2; numeroDeModelo<=(cantidadDeModelos); numeroDeModelo++){
+		int primo = this->devolverPrimo(numeroDeModelo);
+		ModelosSuperiores* modelo = new ModelosSuperiores(numeroDeModelo, primo);
+		this->modelosSuperiores->push_back(modelo);
 	}
+}
+
+int PPMC::devolverPrimo(int numeroDeModelo){
+	int primo;
+		if (numeroDeModelo == 2)
+			primo = 3001;
+		else if (numeroDeModelo == 3)
+			primo = 6001;
+		else if (numeroDeModelo == 4)
+			primo = 7001;
+		else if (numeroDeModelo == 5)
+			primo = 8001; //ES POCO ESPACIO. 8001 SON MENOS DE 150K (SE PUDE AGREGAR MUCHO MAS)
+
+	return primo;
 }
 
 void PPMC::entrenar(string rutaArchivo){
@@ -49,11 +55,14 @@ void PPMC::entrenar(string rutaArchivo){
 //CORREGIR ESTO VA A RECIBIR UN VECTOR<STRING>, NOSE CUANTAS PALABRAS VAN A VENIR
 //TODO DEPENDE DEL PELA!!!!!!
 void PPMC::entrenarPalabras(vector<string>* palabrasLimpias){
-	vector<string>* cincoPalabrasTemporales = this->devolverCincoPalabras(palabrasLimpias, 0, 4);
+	int tamanio = palabrasLimpias->size();
+	vector<string>* cincoPalabrasTemporales;
+	for(int inicio = 0; inicio <= (tamanio - 5); inicio++){
 
-	int tam = cincoPalabrasTemporales->size();
-	string sarasa = "sarasa";
+		cincoPalabrasTemporales = this->devolverCincoPalabras(palabrasLimpias, inicio, inicio+5);
 
+		this->cargarModelos(cincoPalabrasTemporales);
+	}
 	delete cincoPalabrasTemporales;
 
 }
@@ -69,6 +78,42 @@ vector<string>* PPMC::devolverCincoPalabras(vector<string>* palabrasLimpias, int
 		i++;
 	}
 	return cincoPalabrasTemporales;
+}
+
+void PPMC::cargarModelos(vector<string>* cincoPalabrasTemporales){
+
+	vector<string>::iterator it = cincoPalabrasTemporales->end();
+	int numeroDeModelo = this->modelosSuperiores->size();
+
+	while (numeroDeModelo > 0){
+		Palabra* ultimaPalabra = new Palabra(*it);
+		string nombreContexto = "";
+		for(it; it >= cincoPalabrasTemporales->begin(); it--){
+			nombreContexto = *it + " " + nombreContexto;
+		}
+		int primo = this->devolverPrimo(numeroDeModelo);
+
+		//CADA CONTEXTO TIENE EL MISMO PRIMO QUE SU MODELO.
+		//CONTROLAR ESTO PORQUE TAL VEZ NO SEA ASI.
+		//TAL VEZ HAY QUE PONER UN PRIMO MAS CHICO
+
+		Contexto* contextoSuperior = new Contexto(nombreContexto, primo);
+		contextoSuperior->agregarPalabra(ultimaPalabra);
+		this->agregarContextoSuperiorEn(contextoSuperior, numeroDeModelo);
+		it--;
+		numeroDeModelo--;
+	}
+
+}
+
+void PPMC::agregarContextoSuperiorEn(Contexto* unContexto, int numeroDeModelo){
+
+	list<ModelosSuperiores*>::iterator it = this->modelosSuperiores->begin();
+
+	advance(it,(numeroDeModelo - 1));
+
+	ModelosSuperiores* modeloSuperior = *it;
+	modeloSuperior->agregarContexto(unContexto);
 }
 
 PPMC::~PPMC() {
